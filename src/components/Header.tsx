946af80b48e6452e59dd92f059294d31fcbf7902
Isaac "Yak" L <i@isaacyakl.com>
2023-04-30 12:32:04 -0700

    Add a little bit of work

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import ityakicon from "../../public/it-yak-icon.svg";
+
+function Header() {
+	return <header></header>;
+}
+
+export default Header;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+function Hero() {
+	return (
+		<div>
+			<h1>IT Yak</h1>
+			<h2>Providing managed cloud services and consulting</h2>
+			<h3>We handle IT so you can focus on building your business.</h3>
+		</div>
+	);
+}
+
+export default Hero;
